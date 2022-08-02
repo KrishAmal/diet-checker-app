6ebfe00167dbf2c89ebe2a93c89d0faabf28746b
Amal Krishnan <amal.aug95@gmail.com>
2022-08-02 12:12:36 +0530

    Updated with changes for SSG

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "/diet-checker-app",
+  experimental: {
+    images: {
+      unoptimized: true
+    } 
+  }
 }
 
 module.exports = nextConfig
